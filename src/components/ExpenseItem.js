e9824a2f44c80a1f9ebd55371db4eefdeef388d8
Jon McCallum <jonm@roostermarketing.co.uk>
2021-11-18 14:23:37 +0000

    add expense dummy data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 11, 18).toISOString();
+  const expenseTitle = 'Bicycle';
+  const expenseAmount = 200;
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
