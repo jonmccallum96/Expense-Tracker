948b97f7d86cb03d9f7d575ed8c680b1255a6206
Jon McCallum <jonm@roostermarketing.co.uk>
2021-11-18 10:41:01 +0000

    add expense item component file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div> 
       <h1>Expense Tracker</h1>
+      <ExpenseItem />
     </div>
   );
 }
